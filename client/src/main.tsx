61033970a0da62c313c64e45691d6b25d9a94db2
byteful <info@byteful.me>
2025-06-16 03:30:19 -0500

    fixes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,11 +4,14 @@ import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { ThemeProvider } from "next-themes";
+import { Router } from "wouter"; // Import Router from wouter
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <ThemeProvider attribute="class" defaultTheme="dark">
-      <App />
+      <Router> {/* Wrap App with Router */}
+        <App />
+      </Router>
     </ThemeProvider>
   </QueryClientProvider>
 );
